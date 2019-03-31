8d9c07bf585d5cbb88c9181a3b3b685fd3e9e812
Matt Stuhring <whereyouatmatt@gmail.com>
2019-03-31 10:25:29 -0700

    Initialize create-react-app

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(morgan('dev'));
